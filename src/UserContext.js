17c0dfc09c6188d7506c97b7e0a9d91ecffb5377
Hugo Codes <herbtyh@gmail.com>
2023-04-23 10:50:40 -0300

    Adcionar página de cadastro

--- src/UserContext.js
+++ src/UserContext.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { createContext } from 'react';
-import { TOKEN_POST, TOKEN_VALIDATE_POST, USER_GET } from './api';
+import { TOKEN_POST, TOKEN_VALIDATE_POST, USER_GET } from './Api';
 import { useNavigate } from 'react-router-dom';
 
 export const UserContext = createContext();
