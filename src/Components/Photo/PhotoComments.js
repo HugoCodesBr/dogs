f48b4f94cfd560ed6547e5df6b422e4834406bc3
Hugo Codes <herbtyh@gmail.com>
2023-04-24 20:15:36 -0300

    Adicionar seção de comentários

--- src/Components/Photo/PhotoComments.js
+++ src/Components/Photo/PhotoComments.js
@@ -1,7 +1,30 @@
-import React from 'react';
+import React, { useContext, useEffect, useRef, useState } from 'react';
+import { UserContext } from '../../UserContext';
+import PhotoCommentsForm from './PhotoCommentsForm';
+import styles from './PhotoComments.module.css';
 
-const PhotoComments = () => {
-  return <div>PhotoComments</div>;
+const PhotoComments = (props) => {
+  const [comments, setComments] = useState(() => props.comments);
+  const commentSection = useRef(null);
+  const { login } = useContext(UserContext);
+
+  useEffect(() => {
+    commentSection.current.scrollTop = commentSection.current.scrollHeight;
+  }, [comments]);
+
+  return (
+    <>
+      <ul ref={commentSection} className={styles.comments}>
+        {comments.map((comment) => (
+          <li key={comment.comment_ID}>
+            <b>{comment.comment_author}: </b>
+            <span>{comment.comment_content}</span>
+          </li>
+        ))}
+      </ul>
+      {login && <PhotoCommentsForm id={props.id} setComments={setComments} />}
+    </>
+  );
 };
 
 export default PhotoComments;
